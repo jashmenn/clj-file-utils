fa3efac883d25ada3053155e272f4cd0b011f3e9
Nate Murray <nate@natemurray.com>
2010-07-28 11:02:21 -0700

    updated pom and project dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
     :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                    [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                    [commons-io/commons-io "1.4"]]
+    :dev-dependencies [[swank-clojure "1.2.1"]
+                        [clj-unit "0.1.0"]]
     :namespaces [clj-file-utils.core]
-    :repositories {"ibiblio" "http://mirrors.ibiblio.org/pub/mirrors/maven2"}
-)
+    :repositories {"ibiblio" "http://mirrors.ibiblio.org/pub/mirrors/maven2"})
