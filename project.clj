9b35be903e01c56e31d3736767babc65cb49e79d
Nate Murray <nate@natemurray.com>
2010-08-05 16:38:07 -0700

    updated clojure versions

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-file-utils "0.1.1"
-    :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                   [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                   [commons-io/commons-io "1.4"]]
-    :dev-dependencies [[swank-clojure "1.2.1"]
-                        [clj-unit "0.1.0"]]
+(defproject clj-file-utils "0.1.2"
+    :dependencies [[commons-io/commons-io "1.4"]]
+    :dev-dependencies [
+                      [org.clojure/clojure "1.2.0-beta1"]
+                      [org.clojure/clojure-contrib "1.2.0-beta1"]
+                      [swank-clojure "1.2.1"]
+                      [clj-unit "0.1.0"]]
     :namespaces [clj-file-utils.core]
     :repositories {"ibiblio" "http://mirrors.ibiblio.org/pub/mirrors/maven2"})
