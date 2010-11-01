74306903590f83f0ed2948f11d7c0f353d6eebe0
Min Huang <mhuang@attinteractive.com>
2010-11-01 16:51:44 -0700

    Must change versions and all that

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
-(defproject clj-file-utils "0.1.2"
-    :dependencies [[commons-io/commons-io "1.4"]]
-    :dev-dependencies [
-                      [org.clojure/clojure "1.2.0-beta1"]
-                      [org.clojure/clojure-contrib "1.2.0-beta1"]
-                      [swank-clojure "1.2.1"]
-                      [clj-unit "0.1.0"]]
+(defproject clj-file-utils "0.2.0-SNAPSHOT"
+    :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                       [org.clojure/clojure-contrib "1.2.0"]
+                       [swank-clojure "1.2.1"]]
     :namespaces [clj-file-utils.core]
     :repositories {"ibiblio" "http://mirrors.ibiblio.org/pub/mirrors/maven2"})
