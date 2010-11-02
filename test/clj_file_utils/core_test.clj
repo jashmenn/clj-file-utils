a819f4a6104aebaf3af2590c4090aef6b33c8cfc
Min Huang <mhuang@attinteractive.com>
2010-11-01 17:19:51 -0700

    Revamped test suite

--- test/clj_file_utils/core_test.clj
+++ test/clj_file_utils/core_test.clj
@@ -1,25 +1,11 @@
-
 (ns clj-file-utils.core-test
-  (:use clj-unit.core clj-file-utils.core :reload)
+  (:use [clj-file-utils.core] :reload)
+  (:use [clojure.test])
+  (:require [clojure.contrib.io :as io])
   (:import java.io.File))
 
-(def foo-name      "test/clj_file_utils/assets/foo.txt")
-(def fake-foo-name "test/clj_file_utils/assets/fake-foo.txt")
-(def foo      (File. foo-name))
+(def test-file (io/file "test" "clj_file_utils" "core_test.txt"))
 
-(deftest "file"
-  (assert= foo (file foo-name))
-  (assert= foo (file (file "test/clj_file_utils/assets") "foo.txt"))
-  (assert= foo (file "test" "clj_file_utils" "assets" "foo.txt")))
-
-(deftest "mv-exists"
-  (assert-not (exists? fake-foo-name))
-  (assert-not (exists? (file fake-foo-name)))
-  (do (mv foo-name fake-foo-name) true)
-  (assert (exists? fake-foo-name))
-  (assert-not (exists? (file foo-name)))
-  (do (mv fake-foo-name foo-name) true)
-  (assert (exists? foo-name))
-  )
-
-(run-tests 'clj-file-utils.core-test)
+(deftest test-size
+  (is (= 11 (size test-file)))
+  (is (= 11 (size (canonical-path test-file)))))
