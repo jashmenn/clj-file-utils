a1bae4abf9d3da65e82dca0758b290e0b15fe0f7
Min Huang <mhuang@attinteractive.com>
2010-11-05 18:49:15 -0700

    Update version of commons-io

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-file-utils "0.2.0-SNAPSHOT"
   :dependencies
-    [[commons-io/commons-io "1.4"]]
+    [[commons-io/commons-io "2.0"]]
   :dev-dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.clojure/clojure-contrib "1.2.0"]
